2aeb13dcdafb0ddd00790b4b8f6c6eca9d9a3f17
Akshit <akshitjha999999@gmail.com>
2026-04-28 12:54:52 +0530

    Update risk_engine.py

--- backend/risk_engine.py
+++ backend/risk_engine.py
@@ -1,1 +1,15 @@
+def analyze_risk(raw):
+    issues = raw.get("data", {}).get("issues", [])
 
+    score = 0
+    for i in issues:
+        if "CRITICAL" in i:
+            score += 3
+        elif "WARN" in i:
+            score += 1
+
+    return {
+        "risk_score": min(score, 10),
+        "vulnerable": score > 0,
+        "issues": issues
+    }
