0b93223e4249100f0666c12f1b827587408b42ad
Akshit <akshitjha999999@gmail.com>
2026-04-28 11:29:22 +0530

    Updated path_hijack_demo.sh

--- scripts/path_hijack_demo.sh
+++ scripts/path_hijack_demo.sh
@@ -1,16 +1,39 @@
 #!/usr/bin/bash
 
-# Basic path hijack demo script that uses binary planting attack method.
-echo "[*] Simulating PATH hijack...."
+set -e   #exits on error. 
 
-echo -e '#!/bin/bash\necho "PYTHON Hijacked. "' > ./python3
+# Basic path hijack demo script that uses binary planting attack method.
+
+echo "[*] Simulating PATH hijack using Binary Planting"
+
+# Check if python3 exists
+command -v python3 >/dev/null 2>&1 || {
+    echo "python3 not found!, demo may not behave as expected"
+    exit 1
+}
+
+echo "Original python3 location:"
+which python3
+
+#creating the malicious library 
+echo -e '#!/bin/bash\necho "PYTHON EXECUTION HIJACKED."' > ./python3
 chmod +x ./python3
+
+echo "Entering isolated environment (hijack simulation)..."
+
 (
 export PATH=.:$PATH
+
+echo "Hijacked python3 location:"
+which python3
+
+echo "executing python3.."
 python3
 )
 
 rm -f ./python3
+
+echo "DEMO COMPLETE."
 
 
 
