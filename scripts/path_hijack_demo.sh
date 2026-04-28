5a051f9d5d109ad5423572fb808a957431afc911
Akshit <akshitjha999999@gmail.com>
2026-04-28 11:09:21 +0530

    Updated path_hijack_demo.sh

--- scripts/path_hijack_demo.sh
+++ scripts/path_hijack_demo.sh
@@ -1,1 +1,13 @@
+#!/usr/bin/bash
+
+# Basic path hijack demo script that uses binary planting attack method.
+
+echo -e '#!/bin/bash\necho "MALICIOUS CODE THAT IS EXECUTED"' > ./ls
+chmod +x ./ls
+
+export PATH=.:$PATH
+ls
+
+
+
 
